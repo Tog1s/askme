642467483e81dbaebeb8dbdc13f26a79ad0dc9c9
Tog1s <qmax.ku@gmail.com>
2019-03-17 18:02:37 +0300

    Add views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Привет, Мир!"
+    @user = User.new(
+      name: 'Max',
+      username: 'test',
+      avatar_url: 'https://tog1s.github.io/img/robert-downey-Jr.jpg')
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('20.03.2020')),
+      Question.new(text: 'Чё как?', created_at: Date.parse('20.03.2020'))
+    ]
+    @new_question = Question.new
   end
 end

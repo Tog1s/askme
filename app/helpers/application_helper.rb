44fd85c103d4e635b1d6e1fd6b4fcfa1d01e5f0f
maxmuzone <maxmuzone@gmail.com>
2019-06-14 07:06:41 -0400

    Add helper method sklonenie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,16 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+  
+  def sklonenie(number, one, few, many)
+    if(number == nil || !number.is_a?(Numeric))
+      number = 0
+    end
 
+    return many if (number % 100).between?(11, 14)
+    return one if (number % 10) == 1
+    return few if (number % 10).between?(2, 4)
+  
+    return many
+  end
 end
